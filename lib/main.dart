c0e8b78f2cd8b9a95363fcc9d3ea26fea7c3f3e5
Ethan Harstad <ethanharstad@gmail.com>
2024-09-25 13:26:09 -0500

    Enable AppCheck

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:alert/blocs/app_bloc_observer.dart';
 import 'package:alert/repositories/authentication_repository.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:alert/app.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/foundation.dart';
 
 import 'firebase_options.dart';
 
@@ -14,6 +16,12 @@ void main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+    androidProvider: kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
+    appleProvider: kDebugMode ? AppleProvider.debug : AppleProvider.deviceCheck,
   );
 
   final authenticationRepository = AuthenticationRepository();
